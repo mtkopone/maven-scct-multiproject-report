1b22921a6f8dabd623936308e1d25261a7efcd67
Mikko Koponen <koponen@iki.fi>
2012-12-07 16:32:11 +0200

    scala 2.10
    sbt has decided to only use the beginning of the scala version in publish suffixes from 2.10 onward.
    So mangling maven pom to refrect "this fairly simple approach allows interoperability with users of Maven, Ant and other build tools."

--- publish-all.sh
+++ publish-all.sh
@@ -6,16 +6,14 @@ version=0.2-SNAPSHOT
 function install {
   mvn -Dscala.version="${1}" clean package
   sed -e "s/\${scala.version}/${1}/g" -e "s/^.*<scala.version>.*$//g" pom.xml > target/custom-pom.xml
+  sed -i "" -e "s/\${scct.scala.version}/${2}/g" -e "s/^.*<scct.scala.version>.*$//g" target/custom-pom.xml
   mvn deploy:deploy-file \
-    -Durl=file:///Users/mtkopone/projects/scct/gh-pages/maven-repo/ \
+    -Durl=file:///Users/mtkopone/dev/scct-root/gh-pages/maven-repo/ \
     -DrepositoryId=local-gh-pages \
     -Dfile=target/maven-scct-multiproject-report-${version}.jar \
     -DartifactId=maven-scct-multiproject-report_${1} \
     -DpomFile=target/custom-pom.xml
 }
 
-scalas=( "2.9.2" "2.9.1-1" "2.9.1" "2.9.0-1" "2.9.0")
-
-for i in "${scalas[@]}"; do
-  install $i
-done
+install "2.9.2" "2.9.2"
+install "2.10.0-RC3" "2.10"
